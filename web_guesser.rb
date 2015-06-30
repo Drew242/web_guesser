38a751dd8007b5de9fdde78349603ceb23783342
Drew Conly <atconly@gmail.com>
2015-06-30 13:30:12 -0600

    color output working based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -17,11 +17,28 @@ def check_guess(guess)
   end
 end
 
+def color(guess)
+  if guess.to_i == NUM
+    "#00FF00"
+  elsif guess.to_i < NUM - 5
+    "#FF0000"
+  elsif guess.to_i < NUM
+    "#FF5721"
+  elsif guess.to_i > NUM + 5
+    "#FF0000"
+  else guess.to_i > NUM
+    "#FF5721"
+  end
+end
+
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => NUM, :message => message}
+  color = color(guess)
+  erb :index, :locals => {:number => NUM, :message => message, :color => color, :guess => guess}
 end
+
+
 
 
 
